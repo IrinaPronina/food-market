78dba4ac0060e31f2e5af94b420d6d1b58d4ddc9
Irina Pronina <tender52ru@gmail.com>
2023-04-24 13:26:49 +0300

    Pluses modified

--- src/components/Plus/Plus.jsx
+++ src/components/Plus/Plus.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import styles from './plus.module.scss';
-import { ReactComponent as IconDel } from './../../assets/img/scooter.svg';
+// import { ReactComponent as IconDel } from './../../assets/icons/scooter.svg';
+// import { ReactComponent as IconReturn } from './../../assets/icons/return-box.svg';
+// import { ReactComponent as IconSup } from './../../assets/icons/sup.svg';
+
+import IconDel from './../../assets/icons/scooter.svg';
+import IconReturn from './../../assets/icons/return-box.svg';
+import IconSup from './../../assets/icons/sup.svg';
 
 export default function Plus() {
-  const titles = ['Free shipping', 'Purchase returns', '24/7 support'];
+  const pluses = [
+    {
+      title: 'Free shipping',
+      icon: IconDel,
+    },
+    {
+      title: 'Purchase returns',
+      icon: IconReturn,
+    },
+    {
+      title: '24/7 support',
+      icon: IconSup,
+    },
+  ];
+
+  const plusesArr = Object.entries(pluses);
+
   // const ps = ['For orders over $50', 'If the goods have been damaged', 'Top Operators'];
 
   return (
     <section className={styles.plus}>
       <div className={styles.container}>
         <ul className={styles.list}>
-          {titles.map((title, i) => (
-            <li key={i} className={styles.item}>
+          {plusesArr.map(([key, value]) => (
+            <li key={key} className={styles.item}>
               <div className={styles.icon}>
-                <IconDel width="50" height="50" />
+                <img src={value.icon} width="50" height="50" alt="icon" />
               </div>
               <div className={styles.text}>
-                <h3 className={styles.h3}>{title}</h3>
+                <h3 className={styles.h3}>{value.title}</h3>
               </div>
             </li>
           ))}
